0db1e7adbeed56dcf5436fb16e77e7108d9fa763
alexey1254 <alxkill54@gmail.com>
2022-06-04 12:21:22 +0100

    Se quitan cosas

--- src/main/FerreteriaApp.java
+++ src/main/FerreteriaApp.java
@@ -1,14 +1,16 @@
+package main;
 
 import modelo.*;
 import vista.VistaProducto;
 import controlador.ControladorProducto;
 import modelo.Conexion;
 import modelo.ProductoDAO;
-/*** @author Javier Criado, 15/05/2022:20:10:30  ***/
+
+/*** @author Javier Criado, 15/05/2022:20:10:30 ***/
 public class FerreteriaApp {
     public static void main(String args[]) throws Exception {
-        VistaProducto vista=new VistaProducto();
-        ControladorProducto controlador=new ControladorProducto(vista);
+        VistaProducto vista = new VistaProducto();
+        ControladorProducto controlador = new ControladorProducto(vista);
         controlador.inicia();
 
     }
